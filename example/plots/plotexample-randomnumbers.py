2628de84479f51ac6fe028c62190a449c14b67c5
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2017-11-23 16:34:35 +0100

    Added matplotlib examples

---
+++ example/plots/plotexample-randomnumbers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Examples on how to plot using matplotlib
+
+Plot the relative frequency value of a dice
+
+python3 -m pip install matplotlib numpy
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    """
+    This is the main method, I call it main by convention.
+    """
+
+    dice_rolls = 1000
+    rel_freq = []
+    cumulative_value = 0
+    
+    for i in range(1, dice_rolls):
+        value = np.random.randint(1, 6)
+        cumulative_value = cumulative_value + value
+        rel_freq.append(cumulative_value/i)
+
+
+
+    plt.plot(rel_freq, 'g-')
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
